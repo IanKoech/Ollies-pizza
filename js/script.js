6db195e34fab5a8e11e117d20887ee3b5a30ed57
Ian K <iankoech9@gmail.com>
2020-08-03 16:06:09 +0300

    Refractored js file

--- js/script.js
+++ js/script.js
@@ -1,47 +1,64 @@
-/*Js constructor for creating numerous instances*/
-var Pizza=function(size){
-    this.size=size;
-};
+var location;
 var totalPrice=0;
-var deliveryCharge=0;
-var number=parseFloat(document.getElementById("number"));
+var crust=0;
+var toppings=0;
+var size=0;
+var delivery;
+
+var Pizza=function(size,number,crust,delivery,location){
+    this.size=size;
+    this.number=number;
+    this.crust=crust;
+    this.delivery=delivery;
+    this.location=location;
+};
+
+/*Prototyping a method for the pizza constructor*/
+Pizza.prototype.makeOrder=function(){
+    return (this.number+" "+this.size+" of crust type"+this.crust);
+}
+function checkSize(size,number){
+    if(size=="small"){
+        totalPrice+=300;
+    }else if(size=="medium"){
+        totalPrice+=900;
+    }else{
+        totalPrice+=1200;
+    }
+    alert(number);
+    totalPrice=totalPrice*number;
+    alert(number);
+}
+
+function checkDelivery(delivery){
+    if(delivery=="yes"){
+        totalPrice+=200;
+    }else{
+        totalPrice+=0;
+    }
+}
+function checkOut(){
+    return totalPrice;
+}
+
 /*Front-end logic*/
 $(document).ready(function(){
-    
-    
-    $("#button").click(function(){
-        
-        var size=parseFloat(document.getElementsById('size').value);
-        alert(size);
-        if(size=="small"){
-            totalPrice+=300;
-        }else if(size=="medium"){
-            totalPrice+=900;
-        }else{
-            totalPrice+=1200;
-        }
-        totalPrice=totalPrice*number;
-        alert(totalPrice);
-        var delivery=document.getElementById("delivery").value;
-        alert(delivery);
-        var location;
-        if(delivery=="Yes"){
-            deliveryCharge+=200;
-            location=prompt("Enter your delivery location:");
-            alert(location);
-        }else{
-            deliveryCharge+=0;
-        }
-        alert(deliveryCharge);
-        alert("Your delivery will be made to "+location+" at a cost of Ksh:"+deliveryCharge)
+    $("#Yes").click(function(){
+        alert("You will be charged Ksh 200 for delivery");
+        delivery=this.value;
+        location=prompt("Please enter your location");
     });
-    $("#button").submit(function(){
-        var pizzaCrust=document.getElementById("crust").value;
-        var pizzaToppings=document.getElementById("toppings").value;
-        totalPrice+=deliveryCharge;
-        document.getElementById("display-text").innerHTML=totalPrice;
-        document.getElementById("num").innerHTML=number;
+    $("form#form1").submit(function(event){
         event.preventDefault();
+        var size=$("#size").val();
+        var crust=$("#crust").val();
+        var number=parseInt($("#number").val());
+
+        var Customer=new Pizza(size,number,crust,delivery,location);
+        checkDelivery(delivery);
+        checkSize(size,number);
+        checkOut();
+        $("#number").text(number)
+        $("#display-text").text(totalPrice);
     });
-    
 });
