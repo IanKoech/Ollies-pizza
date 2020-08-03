d7a49123cc9ffcc34efd10b3fe93d8116fcec252
Ian K <iankoech9@gmail.com>
2020-08-03 12:20:56 +0300

    Completed js file

--- js/script.js
+++ js/script.js
@@ -1,1 +1,38 @@
-var largePizza=
+/*Front end logic */
+var Pizza=function(size){
+    this.size=size;
+};
+/*Front-end logic */
+
+$(document).ready(function(){
+    var deliveryCharge=0;
+    var totalPrice=0;
+    var toppings=[];
+    var number=parseInt(document.getElementById("number").value());
+    var crust=document.getElementById("crust").value();
+    $("#sizeSelected").click(function(){
+        var pizzaSize=new Pizza(document.getElementsByClassName("sizeSelected").value());
+        if(pizzaSize=="small"){
+            totalPrice+=300;
+        }else if(pizzaSize=="medium"){
+            totalPrice+=900;
+        }else{
+            totalPrice+=1200;
+        }
+        totalPrice*=number;
+    });
+    $(".deliveryD").click(function(){
+        var deliveryDecision=document.getElementById("delivery").value();
+        if(deliveryDecision=="Yes"){
+            prompt("Enter the location of delivery:");
+            prompt("Total delivery charge will be "+"Ksh 250");
+            deliveryCharge=+250;
+        }else{
+            deliveryCharge+=0;
+            break;
+        }
+    });
+    $(".toppings").click(function(){
+        toppings.push(document.getElementsByName("toppings").value());
+    });
+});
